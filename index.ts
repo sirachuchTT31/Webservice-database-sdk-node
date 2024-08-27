710afa5d49d81ad20195448ead1a1923961f373d
sirachuchTT31 <sirachuch.kho@softpomarket.com>
2024-08-27 14:51:31 +0700

    fix : prisma

--- index.ts
+++ index.ts
@@ -1,16 +1,1 @@
-
-// export * from './src/prisma/prisma';
-
-import { PrismaClient } from '@prisma/client'
-
-const prismaClientSingleton = () => {
-  return new PrismaClient()
-}
-
-declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
-} & typeof global;
-
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
-
-export default prisma
+export * from '@prisma/client'
